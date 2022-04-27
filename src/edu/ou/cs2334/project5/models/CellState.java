3ab088e9e2abe513beec0b7d08434d1b5ad0ad61
Thang Quoc Nguyen <thangq.nguyen22@gmail.com>
2022-04-27 14:26:10 -0500

    Created skeleton for cellstate enum

---
+++ src/edu/ou/cs2334/project5/models/CellState.java
@@ -0,0 +1,15 @@
+package edu.ou.cs2334.project5.models;
+
+/**
+ * This enum is used to represent the state of the NonogramModel grid.
+ * 
+ * @author Thang Nguyen
+ *
+ */
+public enum CellState {
+	EMPTY, FILLED, MARKED;
+
+	public toBoolean(state CellState) {
+		
+	}
+}
