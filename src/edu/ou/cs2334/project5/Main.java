fc6182a29dfacfff95f7ed723ca095a58fbbce67
Thang Quoc Nguyen <thangq.nguyen22@gmail.com>
2022-04-27 14:17:48 -0500

    Created skeleton for main class

---
+++ src/edu/ou/cs2334/project5/Main.java
@@ -0,0 +1,14 @@
+package edu.ou.cs2334.project5;
+
+public class Main {
+	private int IDX_CELL_SIZE = 0;
+	private int DEFAULT_CELL_SIZE = 30; 
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	public void start(Stage primaryStage) {
+		
+	}
+}
